9e5966a7b858fd8e14717f8866b0b2d9a34b23d0
siddharth <baghelsiddharth942@gmail.com>
2026-01-21 22:51:13 +0530

    fix: index.ts node

--- node/index.ts
+++ node/index.ts
@@ -2,18 +2,17 @@ import express from 'express'
 import cors from 'cors'
 import dontenv from 'dotenv/config'
 const app = express();
-const apiUrl = process.env.API_URL || "http://localhost:4000";
 const port = Number(process.env.PORT) || 4000;
 
 app.use(express.json())
 app.use(cors({
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
-    origin:["http://localhost:5173","http://locahost:5174","http://localhost:3000",`${apiUrl}`],
+    origin:["http://localhost:5173","http://locahost:5174","http://localhost:3000", "http://43.205.239.77:5174"],
     allowedHeaders:["Content-Type","Authroziation"]
 }))
 
-app.get(`${apiUrl}`,(req,res)=> {
+app.get("/api/check",(req,res)=> {
     res.json({message:"node work!!"})
 })
 
